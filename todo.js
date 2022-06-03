dc6046f488a1d5d65eb43d0c744f81cbe1bee9d4
Dikshyant Dhungana <84063700+dikshyant3@users.noreply.github.com>
2022-06-03 22:36:57 +0545

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,8 +1,26 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const todo = document.getElementById("todos");
+
 
 form.addEventListener('submit', (e) => {
-    e.preventdefault();
+    e.preventDefault();
 
-    const todo = input.value;
+    const todoText = input.value;
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        todoEl.innerText = todoText;
+
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+        });
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+            todoEl.remove();
+        });
+        todo.appendChild(todoEl);
+        input.value = "";
+
+    }
 });
