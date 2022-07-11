879b7651ece6be1061b4dff7edfee87d7d6f0b02
Dikshyant Dhungana <84063700+dikshyant3@users.noreply.github.com>
2022-07-11 23:30:59 +0545

    added local storage in todo list

--- todo.js
+++ todo.js
@@ -1,28 +1,69 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todo = document.getElementById("todos");
+const todosUL = document.getElementById("todos");
 
-function addTodo(){
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const todos = JSON.parse(localStorage.getItem("todos"));
 
-    const todoText = input.value;
-    if (todoText) {
-        const todoEl = document.createElement("li");
-        todoEl.innerText = todoText;
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-        todoEl.addEventListener("click", () => {
-            todoEl.classList.toggle("completed");
-        });
-        todoEl.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-            todoEl.remove();
-        });
-        todo.appendChild(todoEl);
-        input.value = "";
-
-    }
+  addTodo();
 });
-};
-addTodo();
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+
+      updateLS();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl);
+
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
